e28959a441884e5308971364ff07b17900dc44d1
Alexey Poroshin <ufrs12@yandex.ru>
2025-10-16 20:05:36 +0300

    1

--- hx711/hx711.ino
+++ hx711/hx711.ino
@@ -1,35 +1,15 @@
-
-
-void setup() {
-  Serial.begin(9600);
-  
-  // если тарирование при первом запуске -
-  // нужно выждать готовность датчика
-  delay(500);
-  sensor.tare();    // калибровка нуля
-  
-  //sensor.sleepMode(true);		// выключить датчик
-  //sensor.sleepMode(false);	// включить датчик
-}
-
-void loop() {
-  // чтение только по доступности! if available
-
-}
-
-
 #include <ModbusRTUSlave.h>                         //  
-#define MODBUS_SERIAL Serial                       //  ███╗░░░███╗░█████╗░██████╗░██████╗░██╗░░░██╗░██████╗
-#define MODBUS_BAUD 19200                          //  ████╗░████║██╔══██╗██╔══██╗██╔══██╗██║░░░██║██╔════╝
+#define MODBUS_SERIAL Serial                        //  ███╗░░░███╗░█████╗░██████╗░██████╗░██╗░░░██╗░██████╗
+#define MODBUS_BAUD 19200                           //  ████╗░████║██╔══██╗██╔══██╗██╔══██╗██║░░░██║██╔════╝
 #define MODBUS_CONFIG SERIAL_8N1                    //  ██╔████╔██║██║░░██║██║░░██║██████╦╝██║░░░██║╚█████╗░
 #define MODBUS_UNIT_ID 1                            //  ██║╚██╔╝██║██║░░██║██║░░██║██╔══██╗██║░░░██║░╚═══██╗
-const int16_t dePin = 5;                           //  ██║░╚═╝░██║╚█████╔╝██████╔╝██████╦╝╚██████╔╝██████╔╝
+const int16_t dePin = 5;                            //  ██║░╚═╝░██║╚█████╔╝██████╔╝██████╦╝╚██████╔╝██████╔╝
 ModbusRTUSlave modbus(MODBUS_SERIAL, dePin);        //  ╚═╝░░░░░╚═╝░╚════╝░╚═════╝░╚═════╝░░╚═════╝░╚═════╝░
 const uint8_t numHoldingRegisters = 14;             //
 uint16_t holdingRegisters[numHoldingRegisters];     //
 
 #include <GyverHX711.h>
-GyverHX711 sensor(3, 2, HX_GAIN64_A);
+GyverHX711 sensor(3, 2, HX_GAIN32_B);
 // HX_GAIN128_A - канал А усиление 128
 // HX_GAIN32_B - канал B усиление 32
 // HX_GAIN64_A - канал А усиление 64
@@ -39,7 +19,7 @@ void setup() {
   modbus.configureHoldingRegisters(holdingRegisters, numHoldingRegisters);  //  █▀▄▀█ █▀▀█ █▀▀▄ █▀▀▄ █░░█ █▀▀ 
   MODBUS_SERIAL.begin(MODBUS_BAUD, MODBUS_CONFIG);                          //  █░▀░█ █░░█ █░░█ █▀▀▄ █░░█ ▀▀█ 
   modbus.begin(MODBUS_UNIT_ID, MODBUS_BAUD, MODBUS_CONFIG);                 //  ▀░░░▀ ▀▀▀▀ ▀▀▀░ ▀▀▀░ ░▀▀▀ ▀▀▀
-  holdingRegisters[0] = 300;                                                //
+  holdingRegisters[10] = 300;                                                //
 
 }
 
@@ -48,9 +28,9 @@ void setup() {
 void loop() {
   // чтение только по доступности! if available
   if (sensor.available()) {
-    Serial.println(sensor.read());
+    holdingRegisters[10] = sensor.read() / 10;
   }
-  
+
   modbus.poll();
   //analogWrite(3, holdingRegisters[1]);      // отправляем на мосфет
   delay(4);  
